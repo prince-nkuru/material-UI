089754a280026ef03a78d8bb6ae06ec8b0961a28
prince-nkuru <nkuruprinces@gmail.com>
2024-09-11 22:02:03 +0530

    added app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
-import MuiTypography from './components/MuiTypography';
-import MuiButton from './components/MuiButton';
+// import MuiTypography from './components/MuiTypography';
+// import MuiButton from './components/MuiButton';
+// import MuiTextField from './components/MuiTextField';
+// import MuiSelect from './components/MuiSelect';
+import MuiRadioButton from './components/MuiRadioButton';
+import MuiCheckBox from './components/MuiCheckBox';
 
 function App() {
   return (
     <div className="App">
-      <MuiTypography/>
+      {/* <MuiTypography/>
       <MuiButton/>
+      <MuiTextField/>
+      <MuiSelect/> */}
+      <MuiRadioButton/>
+      <MuiCheckBox/>
     </div>
   );
 }
